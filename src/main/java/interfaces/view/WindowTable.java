0bbc3dd3708c3401fa83f5068424f043a7b7820c
Vinicius Ricardo Rosa Noli <viniciusnoli2016@gmail.com>
2022-06-10 19:41:03 -0300

    Finalização

--- src/main/java/interfaces/view/WindowTable.java
+++ src/main/java/interfaces/view/WindowTable.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class WindowTable extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(WindowProduto.class.getResource("table-example.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("table-example.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 675, 600);
         stage.setTitle("Product");
         stage.setScene(scene);
